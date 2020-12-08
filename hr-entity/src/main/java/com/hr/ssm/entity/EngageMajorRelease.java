115d05064e502faee2e5a5dd0006cacfff327a3a
sevenyoungairye <849033901@qq.com>
2020-12-08 18:25:01 +0800

    职位修改的单值查询。

--- hr-entity/src/main/java/com/hr/ssm/entity/EngageMajorRelease.java
+++ hr-entity/src/main/java/com/hr/ssm/entity/EngageMajorRelease.java
@@ -4,6 +4,8 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import java.util.Date;
 import com.baomidou.mybatisplus.annotation.TableId;
 import java.io.Serializable;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -48,14 +50,18 @@ public class EngageMajorRelease implements Serializable {
 
     private String engageType;
 
+    // 响应到页面的格式 应该多8小时
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date deadline;
 
     private String register;
 
     private String changer;
 
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date registerTime;
 
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date changeTime;
 
     private String majorDescribe;
