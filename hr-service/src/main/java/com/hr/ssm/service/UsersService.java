c2f79a03f64adee03fc4983ac13be671d238849e
1592381193 <1592381193@qq.com>
2020-12-08 23:54:25 +0800

    添加了新方法

--- hr-service/src/main/java/com/hr/ssm/service/UsersService.java
+++ hr-service/src/main/java/com/hr/ssm/service/UsersService.java
@@ -1,8 +1,11 @@
 package com.hr.ssm.service;
 
-import com.hr.api.base.CommonResult;
-import com.hr.ssm.entity.Users;
+import com.baomidou.mybatisplus.core.conditions.Wrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.hr.api.base.CommonResult;
+import com.hr.api.base.DataGridView;
+import com.hr.ssm.entity.Users;
 
 /**
  * <p>
@@ -14,6 +17,13 @@ import com.baomidou.mybatisplus.extension.service.IService;
  */
 public interface UsersService extends IService<Users> {
 
+     //用户登录
     CommonResult<Object> doLogin(Users user) throws Exception;
+
+    //用户分页
+    DataGridView getUserByPage(IPage<Users> page, Wrapper<Users> wrapper);
+
+    //查询所有用户
+    CommonResult<Object> findAllUser() throws Exception;
 
 }
