3231a073f78ca87099e96570f6e8ba918bc3f847
sevenyoungairye <849033901@qq.com>
2020-12-10 17:54:48 +0800

    写完了简历管理

--- hr-service/src/main/java/com/hr/ssm/service/EngageResumeService.java
+++ hr-service/src/main/java/com/hr/ssm/service/EngageResumeService.java
@@ -1,5 +1,7 @@
 package com.hr.ssm.service;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.hr.api.base.DataGridView;
 import com.hr.ssm.entity.EngageResume;
 import com.baomidou.mybatisplus.extension.service.IService;
 
@@ -12,5 +14,7 @@ import com.baomidou.mybatisplus.extension.service.IService;
  * @since 2020-12-04
  */
 public interface EngageResumeService extends IService<EngageResume> {
+
+    DataGridView getResumeByPageAndCondition(EngageResume resume, IPage<EngageResume> page);
 
 }
--- hr-service/src/main/java/com/hr/ssm/service/impl/EngageResumeServiceImpl.java
+++ hr-service/src/main/java/com/hr/ssm/service/impl/EngageResumeServiceImpl.java
@@ -1,10 +1,15 @@
 package com.hr.ssm.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.hr.api.base.DataGridView;
 import com.hr.ssm.entity.EngageResume;
 import com.hr.ssm.mapper.EngageResumeMapper;
 import com.hr.ssm.service.EngageResumeService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
 
 /**
  * <p>
@@ -17,4 +22,20 @@ import org.springframework.stereotype.Service;
 @Service
 public class EngageResumeServiceImpl extends ServiceImpl<EngageResumeMapper, EngageResume> implements EngageResumeService {
 
+    @Resource
+    private EngageResumeMapper resumeMapper;
+
+    @Override
+    public DataGridView getResumeByPageAndCondition(EngageResume resume, IPage<EngageResume> page) {
+
+        QueryWrapper<EngageResume> queryWrapper = new QueryWrapper<>();
+
+        queryWrapper.eq("human_major_id", resume.getHumanMajorId()).  // 根据职位名称查询~
+                eq("human_major_kind_id", resume.getHumanMajorKindId()). //  根据职位分类模糊查询~
+                between("register_time", resume.getStartDate(), resume.getEndDate());  // 根据时间查询~ register_time
+
+        IPage<EngageResume> resumeIPage = resumeMapper.selectPage(page, queryWrapper);
+
+        return new DataGridView("", 0, resumeIPage.getTotal(), resumeIPage.getRecords());
+    }
 }
